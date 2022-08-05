ab1d5945b94dfbfbbb1f9e0140fed3dfed74f34c
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-08-04 22:51:21 -0600

    Impresion de variables HTML

--- src/FisrtApp.jsx
+++ src/FisrtApp.jsx
@@ -1,8 +1,15 @@
 
+const getResult = (a,b) => a + b;
+
 export const FisrtApp = () => {
+    const newmessage = {
+        message: 'Hola mundo',
+        title:'alejandra'
+    };
 return (
     <>
-        <h1>Mi primer componente!</h1>
+        <h1>{getResult(5, 9)}</h1>
+        {/*<code>{ JSON.stringify(newmessage) }</code>*/}
         <p>Soy un subtitulo</p>
     </>
   )}
